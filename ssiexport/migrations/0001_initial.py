3ccd24145696be7c5d36f5aa07a5485a420e5a55
Paulo R <proberto.macedo@gmail.com>
2014-10-24 23:30:54 -0200

    Updates migrations

--- ssiexport/migrations/0001_initial.py
+++ ssiexport/migrations/0001_initial.py
@@ -8,6 +8,15 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'Queryset'
+        db.create_table(u'ssiexport_queryset', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ssiexport.URL'])),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+            ('bson_data', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'ssiexport', ['Queryset'])
+
         # Adding model 'Instance'
         db.create_table(u'ssiexport_instance', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
@@ -31,6 +40,8 @@ class Migration(SchemaMigration):
         db.create_table(u'ssiexport_url', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('path', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255, db_index=True)),
+            ('instance', self.gf('django.db.models.fields.related.ForeignKey')(related_name='instance_url_set', null=True, to=orm['ssiexport.Instance'])),
+            ('shtml', self.gf('django.db.models.fields.files.FileField')(max_length=1024)),
         ))
         db.send_create_signal(u'ssiexport', ['URL'])
 
@@ -63,6 +74,9 @@ class Migration(SchemaMigration):
         # Removing unique constraint on 'Instance', fields ['object_id', 'content_type']
         db.delete_unique(u'ssiexport_instance', ['object_id', 'content_type_id'])
 
+        # Deleting model 'Queryset'
+        db.delete_table(u'ssiexport_queryset')
+
         # Deleting model 'Instance'
         db.delete_table(u'ssiexport_instance')
 
@@ -91,28 +105,37 @@ class Migration(SchemaMigration):
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
         u'ssiexport.include': {
-            'Meta': {'object_name': 'Include'},
+            'Meta': {'ordering': "('url', 'template')", 'object_name': 'Include'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'template': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ssiexport.Template']"}),
             'url': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ssiexport.URL']"})
         },
         u'ssiexport.instance': {
-            'Meta': {'unique_together': "(('object_id', 'content_type'),)", 'object_name': 'Instance'},
+            'Meta': {'ordering': "('id',)", 'unique_together': "(('object_id', 'content_type'),)", 'object_name': 'Instance'},
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
         },
+        u'ssiexport.queryset': {
+            'Meta': {'object_name': 'Queryset'},
+            'bson_data': ('django.db.models.fields.TextField', [], {}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ssiexport.URL']"})
+        },
         u'ssiexport.template': {
-            'Meta': {'object_name': 'Template'},
+            'Meta': {'ordering': "('id',)", 'object_name': 'Template'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'md5sum': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'})
         },
         u'ssiexport.url': {
-            'Meta': {'object_name': 'URL'},
+            'Meta': {'ordering': "('path',)", 'object_name': 'URL'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'instance': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'instance_url_set'", 'null': 'True', 'to': u"orm['ssiexport.Instance']"}),
             'instances': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['ssiexport.Instance']", 'symmetrical': 'False'}),
             'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            'shtml': ('django.db.models.fields.files.FileField', [], {'max_length': '1024'}),
             'templates': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['ssiexport.Template']", 'symmetrical': 'False'})
         }
     }
